da5c2c1297b3deea7c5b29d58727f2bd21080409
Sanjeev Rohila <srohila@vmware.com>
2018-07-10 05:40:50 +0530

    adding special methods examples

---
+++ PythonSplMethods/__call__.py
@@ -0,0 +1,25 @@
+class Example(object):
+    def __init__(self):
+        self.factorial = {}
+
+    def __call__(self, value):
+        if value not in self.factorial.keys():
+            print "calling factorial method"
+            self.factorial[value] =  self.fact(value)
+        return self.factorial[value]
+
+
+    def fact(self, number):
+        if number == 1:
+            return 1
+        else:
+            return number * self.fact(number-1)
+
+
+if __name__ == "__main__":
+    e = Example()
+
+    print e(3)
+    print e(4)
+    print e(5)
+    print e(3)
