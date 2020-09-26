750c7480b63832a16af3278361ab3594694922d9
vishal-agarwal11 <38876747+vishal-agarwal11@users.noreply.github.com>
2020-09-26 08:06:41 +0530

    Update red_black_sort.py
    Another version of the program.

--- problems/red_black_sort.py
+++ problems/red_black_sort.py
@@ -1,26 +1,23 @@
-# Sample Input
-#["B", "R", "B", "R", "B", "B", "R", "R", "R"]
-#Sample Output
-#["R", "R", "R", "R", "R", "B", "B", "B", "B"]
-
-
-sample = ["B", "R", "B", "R", "B", "B", "R", "R", "R"]
-
-i = 0
-j = len(sample) - 1
-
-while i <= j:
-    if sample[i] == "B" and sample[j] == "R":
-        sample[i], sample[j] = sample[j], sample[i]
-        i+=1
-        j-=1
-    elif sample[i] == "R" and sample[j] == "R":
-        i+=1
-    elif sample[i] == "B" and sample[j] == "B":
-        j-=1
-    elif sample[i] == "R" and sample[j] == "B":
-        i+=1
-        j-=1
-    else:
-        print("Never come here")
-print(sample)
+def balance(arr):                                                                  
+    if not arr:                                                                    
+        return                                                                     
+                                                                                   
+    N = len(arr)                                                                   
+    i = 0                                                                          
+    j = N-1                                                                        
+                                                                                   
+    while i<j:                                                                     
+        if arr[i] == "R":                                                          
+            i += 1                                                                 
+                                                                                   
+        if arr[j] == "B":                                                          
+            j -= 1                                                                 
+                                                                                   
+        if arr[i] == "B" and arr[j] == "R":                                        
+            arr[i], arr[j] = arr[j], arr[i]                                        
+            i += 1                                                                 
+            j -= 1                                                                 
+                                                                                   
+arr = ["R", "B", "B", "R", "R", "B", "R"]                                          
+balance(arr)                                                                       
+print(arr)
